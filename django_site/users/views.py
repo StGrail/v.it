b84edce2514f5ba131a7bf475f88f20587f89ec6
g <zzmrlol@gmail.com>
2021-01-04 12:47:53 +0300

    Aligh html forms

--- django_site/users/views.py
+++ django_site/users/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from users.forms import UserCreationForm, UserChangeForm
+from users.models import User
+import requests
+import json
+
+# from ..requests_to_hh_api.get_test import get_test
 
 
 def login(request):
@@ -30,8 +35,44 @@ def join(request):
 
 @login_required
 def profile(request):
+    # params = {
+    #     'User-Agent': 'api-test-agent',
+    # }
+
+    # def request_to_hh(experience='noExperience',
+    #                skill='Python',
+    #                area='Москва',
+    #                salary_from=''):
+    #     if skill != 'Python':
+    #         params['text'] = f'Python+AND+{skill}'
+    #     else:
+    #         params['text'] = skill
+    #     if area == 'Москва':
+    #         params['area'] = 1
+    #     if salary_from != '':
+    #         params['only_with_salary'] = 'true'
+    #         params['salary'] = f'{str(salary_from)}&from'
+    #     else:
+    #         salary_from = salary_from
+
+    #     vacancy = requests.get('https://api.hh.ru/vacancies', params=params)
+    #     vacancy_url = requests.utils.unquote(vacancy.url)
+    #     vacancy_text = requests.get(vacancy_url).text
+    #     vacancy_json = json.loads(vacancy_text)
+    #     # for item in vacancy_json['items']:
+    #     #     print(item['name'])
+    #     #     print(item['alternate_url'])
+    #     #     print(item['salary'])
+    #     #     print(item['type'])
+    #     print(vacancy_url)
+    #     return vacancy_url
+
+    # request_to_hh(experience=User.experience,
+    #               area=User.area,
+    #               salary_from=User.salary)
     context = {
         'title': 'Your profile',
+        # 'vacancy_url': vacancy_url,  #  Сделать табличку в базе и туда писать вакансии
     }
     return render(request, 'profile.html', context)
 
