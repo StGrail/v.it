39aced5519a3b4ef5b17353cff7f52b6f3f91072
Alexey Pakhomov <alexeyt101@mail.ru>
2021-01-11 22:22:31 +0300

    Added code that parse and saving vacancy's skills to the database

--- django_site/parser_vacancies/management/commands/checking_vacancy_for_skills.py
+++ django_site/parser_vacancies/management/commands/checking_vacancy_for_skills.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
-from parser_vacancies.management.commands import config
+from parser_vacancies.management.commands import config_parser
 
 
 def processing_long_skill_name(skill_list):
     for skill in skill_list:
-        if skill in config.PROCESSING_SKILLS:
+        if skill in config_parser.PROCESSING_SKILLS:
             return skill
 
 
@@ -13,7 +13,31 @@ def is_skill_in_list(vacancy):
     Функция принимает на вход полное представление вакансии в виде словаря. 
     Возвращает словарь скиллов, содержащихся в ключевых навыках и описании вакансии
     '''
-    skills = {}
+    contains_skills = False
+    skills = {
+            'sql': False,
+            'linux': False,
+            'git': False,
+            'postgresql': False,
+            'django': False,
+            'flask': False,
+            'css': False,
+            'html': False,
+            'bash': False,
+            'mysql': False,
+            'oop': False,
+            'docker': False,
+            'mongodb': False,
+            'pandas': False,
+            'numpy': False,
+            'jira': False,
+            'kubernetes': False,
+            'http': False,
+            'tcp_ip': False,
+            'trello': False,
+            'unix': False,
+            'redis': False,
+            }
     vacancy_skill_list = vacancy['key_skills']
     for skill_dict in vacancy_skill_list:
         skill = skill_dict['name'].lower()
@@ -22,11 +46,12 @@ def is_skill_in_list(vacancy):
             skill = processing_long_skill_name(skill_list)
         else:
             skill = skill_list[0]
-        if skill in config.PROCESSING_SKILLS:
-            skills[skill] = skill
+        if skill in config_parser.PROCESSING_SKILLS:
+            skills[skill] = True
+            contains_skills = True
     description = vacancy['description']
     description = BeautifulSoup(description, 'html.parser').text.lower()
-    for skill in config.PROCESSING_SKILLS:
+    for skill in config_parser.PROCESSING_SKILLS:
         if description.find(skill) != -1:
-            skills[skill] = skill
-    return skills     
+            skills[skill] = True
+    return skills, contains_skills      
--- django_site/parser_vacancies/management/commands/parser.py
+++ django_site/parser_vacancies/management/commands/parser.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand
+from django.db.utils import IntegrityError
 from users.models import Vacancies
 from parser_vacancies.management.commands import checking_vacancy_for_skills as cv
-from parser_vacancies.management.commands import config
+from parser_vacancies.management.commands import config_parser
 from parser_vacancies.models import Skills
 
 import json
@@ -15,17 +16,16 @@ class Command(BaseCommand):
         return json.loads(answer_hh)
 
 
-    def get_vacancy_data(self, id_vacancy, skills_dict, vacancy):
+    def get_vacancy_data(self, id_vacancy, vacancy, contains_skills):
         '''
         Принмает на вход id вакансии, индикатор наличия скиллов, полное представление вакансии также ввиде словаря.
         Возвращает словарь полей вакансии, которые будут записаны в таблицу vacancies БД.
         '''
         vacancy_data = {'id_vacancy': id_vacancy}
-        if skills_dict:
+        if contains_skills:
             vacancy_data['contains_skills'] = True
         else:
             vacancy_data['contains_skills'] = False
-        vacancy_data['skills'] = list(skills_dict.keys())
         vacancy_data['experience'] = vacancy['experience']['name'].lower()
         if vacancy['salary'] and vacancy['salary']['from']:
             vacancy_data['salary_from'] = vacancy['salary']['from']
@@ -42,12 +42,13 @@ class Command(BaseCommand):
         return vacancy_data
 
 
-    def save_vacancy_data_in_db(self, vacancy_data):
+    def save_vacancy_data_in_db(self, vacancy_data, skills_dict):
         '''
         Принимает на вход поля вакансии, подлежащие сохранению в БД в виде словаря
         Выполняет сохранение вакансии в БД
         '''
-        row = Vacancies(id_vacancy=vacancy_data['id_vacancy'],
+        row_vacancy = Vacancies(
+                        id_vacancy=vacancy_data['id_vacancy'],
                         name=vacancy_data['name'],
                         area=vacancy_data['area'],
                         experience=vacancy_data['experience'],
@@ -57,7 +58,38 @@ class Command(BaseCommand):
                         published=vacancy_data['published'],
                         contains_skills=vacancy_data['contains_skills'],
                         )
-        row.save()
+        try:
+            row_vacancy.save()
+        except IntegrityError:
+            print(f"Vacancy with id {vacancy_data['id_vacancy']} is contained in the database")
+            return
+        if vacancy_data['contains_skills']:
+            row_vacancy_skills = Skills(
+                        id_vacancy=row_vacancy,
+                        sql=skills_dict['sql'],
+                        linux=skills_dict['linux'],
+                        git=skills_dict['git'],
+                        postgresql=skills_dict['postgresql'],
+                        django=skills_dict['django'],
+                        flask=skills_dict['flask'],
+                        css=skills_dict['css'],
+                        html=skills_dict['html'],
+                        bash=skills_dict['bash'],
+                        mysql=skills_dict['mysql'],
+                        oop=skills_dict['oop'],
+                        docker=skills_dict['docker'],
+                        mongodb=skills_dict['mongodb'],
+                        pandas=skills_dict['pandas'],
+                        numpy=skills_dict['numpy'],
+                        jira=skills_dict['jira'],
+                        kubernetes=skills_dict['kubernetes'],
+                        http=skills_dict['http'],
+                        tcp_ip=skills_dict['tcp_ip'],
+                        trello=skills_dict['trello'],
+                        unix=skills_dict['unix'],
+                        redis=skills_dict['redis'],
+                        )
+            row_vacancy_skills.save()
 
 
     def processing_vacancies_in_page(self, short_vacancies):
@@ -76,22 +108,18 @@ class Command(BaseCommand):
                 print('Глубина выборки не более 2000 вакансий')
                 break
             vacancy = self.get_request_data(f'https://api.hh.ru/vacancies/{vacancy_id}')
-            skills_dict = cv.is_skill_in_list(vacancy)
+            skills_dict, contains_skills = cv.is_skill_in_list(vacancy)
             #if skills_dict:
             #    contains_skills = True
-            vacancy_data = self.get_vacancy_data(vacancy_id, skills_dict, vacancy)
-            self.save_vacancy_data_in_db(vacancy_data)
+            vacancy_data = self.get_vacancy_data(vacancy_id, vacancy, contains_skills)
+            self.save_vacancy_data_in_db(vacancy_data, skills_dict)
 
 
     def handle(self, *args, **kwargs):
         count_vacancies = 0
         print('Start vacancies parser')
-        for page in range(config.REQUEST_PAGE_COUNT):
-            short_vacancies = self.get_request_data(f'{config.REQUEST_URL}{page}')
+        for page in range(config_parser.REQUEST_PAGE_COUNT):
+            short_vacancies = self.get_request_data(f'{config_parser.REQUEST_URL}{page}')
             count = self.processing_vacancies_in_page(short_vacancies)
             count_vacancies += len(short_vacancies['items'])
         print('Vacancies parser complited')
-        print(f'Added to db {count_vacancies} vacancies')
-            
-
-
