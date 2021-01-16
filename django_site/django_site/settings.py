54d52cea5cfd626bb96e458d39ae2d20aa406fa5
g <zzmrlol@gmail.com>
2021-01-16 19:37:33 +0300

    Add constants to config.py

--- django_site/django_site/settings.py
+++ django_site/django_site/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import psycopg2.extensions
 from pathlib import Path
-import os
-# import config
+import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +22,7 @@ V_IT_BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('DJANGO_SECRET_KEY'))
-
-print(os.getenv)
+SECRET_KEY = config.DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,8 +83,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django_test',
-        'USER': 'django_test',  # config.PG_USER,
-        'PASSWORD': 'test1234',  # config.PG_PASSWORD,
+        'USER': config.PG_USER,
+        'PASSWORD': config.PG_PASSWORD,
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -141,6 +138,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = str(os.getenv('EMAIL_USER'))
-EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_PASSWORD'))
+EMAIL_HOST_USER = config.EMAIL_USER
+EMAIL_HOST_PASSWORD = config.EMAIL_PASSWORD
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
