7a8023c749e0adf03286c4da16d9a05e919802fe
g <zzmrlol@gmail.com>
2021-02-09 18:36:36 +0300

    Change serializers tests

--- django_site/api/tests/test_serializers.py
+++ django_site/api/tests/test_serializers.py
@@ -8,18 +8,18 @@ class UsersSerializerTestCase(TestCase):
     def test_is_ok(self):
         """ Поменять пароль на другие данные и сравнить, тк хэш """
 
-        user_1 = User.objects.create_user(email='foo1@bar.foo', password='foobar')
-        user_2 = User.objects.create_user(email='foo2@bar.foo', password='foobar')
+        user_1 = User.objects.create_user(email='user_1@bar.com', password='foobar')
+        user_2 = User.objects.create_user(email='user_2@bar.com', password='foobar')
         data = UsersSerializer([user_1, user_2], many=True).data
         expected_data = [
             {
                 'id': user_1.id,
-                'email': 'foo1@bar.foo',
+                'email': 'user_1@bar.com',
                 'password': 'foobar'
             },
             {
                 'id': user_2.id,
-                'email': 'foo2@bar.foo',
+                'email': 'user_2@bar.com',
                 'password': 'foobar'
             },
 
--- django_site/api/views.py
+++ django_site/api/views.py
@@ -1,10 +1,11 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import OrderingFilter
 from rest_framework import viewsets
-
-from api.serializers import UsersSerializer, VacanciesSerializer
-from users.models import User
+from rest_framework.filters import OrderingFilter
 from vacancies.models import Vacancies
+
+
+from users.models import User
+from .serializers import UsersSerializer, VacanciesSerializer
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
@@ -17,13 +18,11 @@ class VacanciesViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = VacanciesSerializer
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filter_fields = [
-        'id_vacancy',
         'name',
         'area',
         'experience',
         'salary_from',
         'salary_to',
-        'url',
         'published',
     ]
     ordering_fields = ['published']
