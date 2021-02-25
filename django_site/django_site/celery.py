dafb505d8fba0817b124453997bb70e2637a130c
g <zzmrlol@gmail.com>
2021-02-25 20:13:55 +0300

    Change scheduler once per day

--- django_site/django_site/celery.py
+++ django_site/django_site/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
 import django
 from django.conf import settings
 
@@ -20,7 +21,7 @@ def debug_task(self):
 app.conf.beat_schedule = {
     'add-every-6-hours': {
         'task': 'vacancies.tasks.parse',
-        'schedule': 360.0,
+        'schedule': crontab(minute=0, hour=0),  # каждый день в полночь
         'args': ()
     },
 }
