caee830c9889dad7771d64dd66e26127cca2758d
g <zzmrlol@gmail.com>
2021-02-25 19:13:42 +0300

    Add data to .env

--- django_site/django_site/settings.py
+++ django_site/django_site/settings.py
@@ -179,10 +179,10 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 django_heroku.settings(locals())
 
 # Celery
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
+CELERY_BROKER_URL = str(os.getenv('CELERY_BROKER'))
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
+CELERY_RESULT_BACKEND = str(os.getenv('CELERY_BROKER'))
 CELERY_TIMEZONE = 'UTC'
 
 
