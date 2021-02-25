76edc0daba00d46dc7bedc641c9255e6fea7e73f
g <zzmrlol@gmail.com>
2021-02-25 18:15:20 +0300

    Add django-redis

--- django_site/django_site/settings.py
+++ django_site/django_site/settings.py
@@ -179,8 +179,20 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 django_heroku.settings(locals())
 
 # Celery
-CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://localhost:6379/1'
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_RESULT_BACKEND = 'redis'
 CELERY_TIMEZONE = 'UTC'
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://localhost:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "MAX_ENTRIES": 1000
+        },
+    }
+}
