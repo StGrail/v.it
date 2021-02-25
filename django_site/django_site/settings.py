5ed3c2831262bfc12a452902c83b5d835aa39b71
g <zzmrlol@gmail.com>
2021-02-25 19:41:57 +0300

    Change redis_url

--- django_site/django_site/settings.py
+++ django_site/django_site/settings.py
@@ -179,17 +179,17 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 django_heroku.settings(locals())
 
 # Celery
-CELERY_BROKER_URL = str(os.getenv('CELERY_BROKER'))
+CELERY_BROKER_URL = str(os.getenv('REDIS_URL'))
+CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_RESULT_BACKEND = str(os.getenv('CELERY_BROKER'))
 CELERY_TIMEZONE = 'UTC'
 
 
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://localhost:6379/1",
+        "LOCATION": str(os.getenv('CELERY_BROKER')),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "MAX_ENTRIES": 1000
