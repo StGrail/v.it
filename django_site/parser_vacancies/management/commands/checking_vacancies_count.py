15cd57a7cb96a9eb3b61551445a8b9fa6d675d15
Alexey Pakhomov <alexeyt101@mail.ru>
2021-03-02 21:50:42 +0300

    Added some docstrings

--- django_site/parser_vacancies/management/commands/checking_vacancies_count.py
+++ django_site/parser_vacancies/management/commands/checking_vacancies_count.py
@@ -6,7 +6,12 @@ from parser_vacancies.management.commands import config_parser
 
 
 
-def check_vacancies_table():
+def check_vacancies_table() -> str:
+    """
+    Функция, которая определяет наличие вакансий в БД и возвращает 
+    URL для запроса
+
+    """
     vacancies_count = Vacancies.objects.filter().count()
     if vacancies_count:
         url_to_parser = config_parser.REQUEST_URL_PERIOD
@@ -16,7 +21,11 @@ def check_vacancies_table():
     return url_to_parser, vacancies_count
 
 
-def save_vacancies_count_to_db(added_today, total_vacancies_count):
+def save_vacancies_count_to_db(added_today: int, total_vacancies_count: int):
+    """
+    Функция, которая сохраняет в БД количество вакансий, сохраненных в БД при очередном включении
+    парсера, дату включения, а также общее количество вакансий в БД
+    """
     row_vacancies_count = Vacancies_count(
         date=date.today(),
         added_today=added_today,
