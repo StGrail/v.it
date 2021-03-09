8ceb1ac2ae041fdc7302c6cefcbea8128306ba67
g <zzmrlol@gmail.com>
2021-03-09 20:40:51 +0300

    Add request to vacancies_counter

--- django_site/web_pages/context_processors.py
+++ django_site/web_pages/context_processors.py
@@ -3,7 +3,7 @@ import datetime
 from parser_vacancies.models import Vacancies_count
 
 
-def vacancies_counter():
+def vacancies_counter(request):
     try:
         qs_counter = Vacancies_count.objects.filter(date=datetime.date.today()).values('added_today',
                                                                                        'total_vacancies_count')
