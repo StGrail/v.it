deb0b4275fce22a0b8dccce57b8dbda2a0a4a631
G <zzmrlol@gmail.com>
2021-04-19 14:05:31 +0300

    Change django_site to core in Procfile

--- django_site/core/celery.py
+++ django_site/core/celery.py
@@ -8,7 +8,7 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-app = Celery('core')
+app = Celery('django_site')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
